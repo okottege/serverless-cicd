dec634362f93594b99fbe3b088e9a4b0eefa9d6e
Oshan Kottege <oshan.kottege@gmail.com>
2020-05-16 11:57:19 +1200

    Initial commit.

---
+++ handler.js
@@ -0,0 +1,12 @@
+const moment = require('moment');
+
+module.exports.logger = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Serverless CI / CD Demo',
+      version: 'v1.0',
+      timestamp: moment().unix()
+    })
+  };
+};
