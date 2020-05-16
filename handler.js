a176b438a1cdb14ff1ede6fd9947a5da56ac2d76
Oshan Kottege <oshan.kottege@gmail.com>
2020-05-16 12:11:56 +1200

    Changed the version number.

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.logger = async event => {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Serverless CI / CD Demo',
-      version: 'v1.0',
+      version: 'v2.0',
       timestamp: moment().unix()
     })
   };
